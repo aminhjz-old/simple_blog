16f1e10a81d320c36c61d75ea128afa6fae82a62
amin <amin.hedjazi@gmail.com>
2012-03-03 13:37:26 +0330

    intializing projects repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleBlog::Application.config.secret_token = 'dfb33bbc48e0419269b73386fb95782fb8f79dfb7837ded8978435efacc96a2c8608dd88580e13b5dfe439c4553f82dd0df556c6ac36f88ffa7084bbb80f6a5a'
